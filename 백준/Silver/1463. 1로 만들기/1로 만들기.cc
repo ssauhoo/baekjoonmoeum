89686a2d25de903475ca5e5fb47406870a09ec22
ssauhoo <92290012+ssauhoo@users.noreply.github.com>
2025-08-23 13:04:25 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int d[1000001];
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin>>n;
+    d[1]=0;
+    for(int i=2;i<=n;i++){
+        d[i]=d[i-1]+1;
+        if(i%2==0 && d[i]>d[i/2]+1) d[i]=d[i/2]+1;
+        if(i%3==0 && d[i]>d[i/3]+1) d[i]=d[i/3]+1;
+    }
+    cout<<d[n];
+}
+
